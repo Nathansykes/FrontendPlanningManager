14684374ca8a7cb2ef933bbc4fdb746381395bfc
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2022-12-07 21:50:58 +0000

    collabartive view

--- src/components/Collaboration.vue
+++ src/components/Collaboration.vue
@@ -1,50 +1,53 @@
 <template>
+  <br />
   <div class="row">
     <div class="col-md-2">
       <h3 class="text-center">Schedulers</h3>
       <br />
       <div :ref="'schedulerContainer'" style="overflow-y: scroll; height: 78vh">
-        <div v-for="scheduler in this.schedulers" :key="scheduler.id" :value="scheduler.id" class="card bg-primary mb-3 scheduler-card" :data-event="JSON.stringify(scheduler)">
+        <div v-for="scheduler in this.schedulers" :key="scheduler.id" :value="scheduler.id"
+          class="card bg-primary mb-3 scheduler-card" :data-event="JSON.stringify(scheduler)">
           <div class="card-header" @click="setSelectedUser(scheduler.id)">{{ scheduler.name }}</div>
           <div class="card-body" @click="setSelectedUser(scheduler.id)">
             <p class="card-text">
-              {{ scheduler.id }} <br />
-              {{ scheduler.name }} <br />
+              Last Online: 1 min ago <br />
             </p>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-md-8">
-      <div id="peterHistory" v-if="(selectedUser==1)">
-        <!-- chats go here -->
-        <p class="peter"></p>
-        <p class="user1"></p>
+    <div class="col-md-10">
+      <div class="row">
+        <div class="col-md-11">
+            <h3>Chatting to {{this.schedulers.find(s => s.id == this.selectedUser).name}}</h3>
+        </div>
+        <div class="col-md-1">
+          <a :href="('callto:07' + Math.floor(Math.random() * 9999999999))" class="btn btn-lg btn-success" title="Call"><i class="bi bi-telephone-outbound"></i></a>
+          </div>
       </div>
-      <div id="johnHistory" v-if="(selectedUser==2)">
-        <!-- chats go here -->
-      </div>
-      <div id="louiseHistory" v-if="selectedUser==3">
-        <!-- chats go here -->
-      </div>
-      <form class="chat">
 
-        <div v-if="(selectedUser==1)">
-          <input id="PeterSmythInput" type="text" placeholder="Message Peter"/>
-          <input type="button" @click="this.peterChat()" value="submit" />
+      <div id="messageContainerParent">
+        <div :id="`messageContainer-${this.selectedUser}`" v-if="(this.selectedUser == 1)">
+          <!-- chats go here -->
+        </div>
+        <div :id="`messageContainer-${this.selectedUser}`" v-if="(this.selectedUser == 2)">
+          <!-- chats go here -->
+        </div>
+        <div :id="`messageContainer-${this.selectedUser}`" v-if="this.selectedUser == 3">
+          <!-- chats go here -->
         </div>
 
-        <div v-if="(selectedUser==2)">
-         <input id="JohnConnorsInput" type="text" placeholder="Message John"/>
-         <input type="button" @click="this.johnChat()" value="submit" />
-        </div>
-
-        <div v-if="(selectedUser==3)">
-          <input id="LouisePitcherInput" type="text" placeholder="Message Louise" />
-          <input type="button" @click="this.louiseChat()" value="submit" />
-        </div>
-
-      </form>
+        <form class="chat" v-on:submit.prevent="sendChat">
+          <h3 class="hidden" id="typing">{{this.typing}}</h3>
+          <div class="input-group mb-3">
+            <input required v-model="this.userInput" type="text" class="form-control" placeholder="Type Message"
+              maxlength="100" />
+            <div class="input-group-append">
+              <button class="btn btn-primary" type="submit">Send</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -60,55 +63,48 @@ export default {
   data() {
     return {
       schedulers: Schedulers,
-      responses1: PeterSmythResponses,
-      responses2: JohnConnorsResponses,
-      responses3: LouisePitcherResponses,
-      count1: 0,
-      count2: 0,
-      count3: 0,
-      selectedUser: 0,
+      selectedUser: 1,
+      userInput: '',
+      typing: '.',
     };
   },
   methods: {
-    peterChat() {
-      var userInput = document.getElementById("PeterSmythInput").value;
-      document.getElementById("peterHistory").append(userInput);
-      this.count1++;
+    sendChat() {
+      this.$message('You', this.userInput, new Date(), true, this.selectedUser);
+      this.userInput = '';
+      let response;
 
-      console.log("hit");
-      this.responses1.forEach((response) => {
-        if (response.id == this.count1) {
-          console.log(response.body)
-          document.getElementById("peterHistory").append(response.body);
-        }
-      });
-    },
-    johnChat() {
-      var userInput = document.getElementById("JohnConnorsInput").value;
-      document.getElementById("johnHistory").append(userInput);
-      this.count2++;
+      document.getElementById('typing').classList.remove('hidden');      
 
-      this.responses2.forEach((response) => {
-        if (response.id == this.count2) {
-          document.getElementById("johnHistory").append(response.body);
+      setTimeout(() => {
+        switch (this.selectedUser) {
+          case 1:
+            response = PeterSmythResponses[Math.floor(Math.random() * 3)];
+            this.$message('Peter', response.body, new Date(), false, this.selectedUser);
+            break;
+          case 2:
+            response = JohnConnorsResponses[Math.floor(Math.random() * 3)];
+            this.$message('John', response.body, new Date(), false, this.selectedUser);
+            break;
+          case 3:
+            response = LouisePitcherResponses[Math.floor(Math.random() * 3)];
+            this.$message('Louise', response.body, new Date(), false, this.selectedUser);
+            break;
         }
-      });
-    },
-    louiseChat() {
-      var userInput = document.getElementById("LouisePitcherInput").value;
-      document.getElementById("louiseHistory").append(userInput);
-      this.count3++;
-
-      this.responses3.forEach((response) => {
-        if (response.id == this.count3) {
-          document.getElementById("louiseHistory").append(response.body);
-        }
-      });
+        document.getElementById('typing').classList.add('hidden');
+      }, 2000);
     },
     setSelectedUser(userId) {
       this.selectedUser = userId;
-      console.log("selected user is " + this.selectedUser);
-    },
+    }, 
   },
+  mounted(){
+    setInterval(() => {
+        this.typing = this.typing + '.';
+        if (this.typing.length > 6) {
+          this.typing = '.';
+        }
+      }, 200);
+  }
 };
 </script>
---
+++ src/components/Message.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="toast show message-toast" :class="self ? 'float-end' : ''" role="alert" aria-live="assertive" aria-atomic="true" >
+                <div class="toast-header">
+                  <strong class="me-auto">{{title}}</strong>
+                  <small>{{(Math.round((((new Date() - sent) % 86400000) % 3600000) / 60000))}} mins ago</small>
+                  <button type="button" class="btn-close ms-2 mb-1" data-bs-dismiss="toast" aria-label="Close">
+                    <span aria-hidden="true"></span>
+                  </button>
+                </div>
+                <div class="toast-body">
+                  {{msg}}
+                </div>
+              </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Message',
+    props: {
+        title: String,
+        msg: String,
+        sent: Date,
+        self: Boolean
+    },
+}
+
+</script>
